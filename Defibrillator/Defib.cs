2bd58b2f86a360d904c8eab1b1ac84bd6f1eba5f
Dániel Mihály <rikkiardo3@gmail.com>
2021-03-09 18:52:59 +0100

    Add project files.

---
+++ Defibrillator/Defib.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defibrillator
+{
+    class Defib
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        private double latitude;
+
+        public double Latitude
+        {
+            get { return latitude; }
+        }
+
+        private double longitude;
+
+        public double Longitude
+        {
+            get { return longitude; }
+        }
+
+        private string tel;
+
+        public string Tel
+        {
+            get { return tel; }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+        }
+
+        private double dist;
+
+        public double Dist
+        {
+            get { return dist; }
+        }
+
+        public Defib(int id, string name, double latitude, double longitude, string tel, string address, double dist)
+        {
+            this.id = id;
+            this.name = name;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.tel = tel;
+            this.address = address;
+            this.dist = dist;
+        }
+    }
+}
---
+++ Defibrillator/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Defibrillator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKilepes = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnKilepes
+            // 
+            this.btnKilepes.Location = new System.Drawing.Point(257, 559);
+            this.btnKilepes.Name = "btnKilepes";
+            this.btnKilepes.Size = new System.Drawing.Size(115, 40);
+            this.btnKilepes.TabIndex = 0;
+            this.btnKilepes.Text = "Kilépés";
+            this.btnKilepes.UseVisualStyleBackColor = true;
+            this.btnKilepes.Click += new System.EventHandler(this.btnKilepes_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 559);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Új adat megadása";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 201);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(360, 304);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 611);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnKilepes);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Defibrillátor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKilepes;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
